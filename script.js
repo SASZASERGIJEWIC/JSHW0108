22000c5e26cc6d99e15f3278cb89f325d2ab50ca
SASZASERGIJEWIC <129986186+SASZASERGIJEWIC@users.noreply.github.com>
2023-08-07 21:50:05 +0200

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function checkDataType(arg) {
   const dataType = typeof arg;
+ 
   const message =
     dataType === "object" && Array.isArray(arg) ? "array" : dataType;
 
   console.log(`Тип данных аргумента: ${message}`);
 }
 
-checkDataType(10); // Тип данных аргумента: number
-checkDataType([98, 220, 34]); // Тип данных аргумента: array
-checkDataType("Hi"); // Тип данных аргумента: string
-checkDataType(null); // Тип данных аргумента: object
-checkDataType(true); // Тип данных аргумента: boolean
-checkDataType({}); // Тип данных аргумента: object
+checkDataType(10);
+checkDataType([98, 220, 34]); 
+checkDataType("Hi"); 
+checkDataType(null); 
+checkDataType(true); 
+checkDataType({}); 
